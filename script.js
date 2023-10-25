d363d940468bf0261ec39e4231d50ce86e81606c
mihirheda02 <87570498+mihirheda02@users.noreply.github.com>
2023-10-24 18:37:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    console.log("loaded");
+    
     const downloadButton = document.querySelector('.download-button');
     const openBase = document.querySelector('.open-base');
 
@@ -6,7 +8,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const isAndroid = /Android/.test(navigator.userAgent);
 
     const urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams);
     const communityId = urlParams.get('communityId');
+    console.log(communityId);
 
     if (isIOS) {
         downloadButton.textContent = 'Download on the App Store';
@@ -18,9 +22,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         downloadButton.textContent = 'Download Now'; // Default for non-mobile devices
         downloadButton.href = '#';
     }
-
-    console.log(urlParams);
-    console.log(communityId);
 
     if (communityId) {
         openBase.href = 'base://' + communityId;
